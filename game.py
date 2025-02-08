d78343abda6c98850108ad991e68b3a4071d0c9c
Ensup Choi <ensup@vivaldi.net>
2025-02-08 23:58:33 +0900

    Complete match system

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import random
 class QueueList(list): #class for List + Queue
     def enqueue(self, item):
         self.append(item)
@@ -6,7 +7,7 @@ class QueueList(list): #class for List + Queue
     def find_optimal(self):
         idx = 1
         for i in range(2, len(self)):
-            if self[i]-self[0] < self[idx]-self[0]:
+            if abs(self[i].score-self[0].score) < abs(self[idx].score-self[0].score):
                 idx = i
         return idx
 class Sys: #Class of game system
@@ -18,12 +19,12 @@ class Sys: #Class of game system
     def add_queue(self):
         for player in self.players:
             self.queue.enqueue(player)
-    def matchmake(self):
+    def match(self):
         idx = self.queue.find_optimal()
         player2 = self.queue[idx]
         del self.queue[idx]
         player1 = self.queue.dequeue()
-        return player1, player2
+        do_match((player1, player2), self)
 
 class Player: #Save player data
     def __init__(self, score):
@@ -31,15 +32,43 @@ class Player: #Save player data
         self.score = score
         self.time = 0
     def __str__(self):
-        data = 'Initial score: ' + str(self.init_score) + ', Current score: ' + str(self.score) + \
+        data = 'Initial score: ' + str(self.init_score) + ', Current score: ' + str(int(self.score)) + \
             ', Played time: ' + str(self.time)
         return data
-def match(players:tuple,game:Sys):
+def do_match(players:tuple,game:Sys):
     """
     Input: Tuple(Player, Player), Sys
     """
     p1, p2 = players
-    #승패 가르기
-    #점수 반영하기
+    #승률 계산
+    prob1 = 1 / (1 + 10 ** ((p2.score - p1.score) / 400))
+    prob2 = 1 - prob1
+    # Update the Elo ratings
+    k=32
+    if random.random() < prob1:
+        # Player1 wins
+        p1.score += k * (1 - prob1)
+        p2.score += k * (0 - prob2)
+    else:
+        # Player2 wins
+        p1.score += k * (0 - prob1)
+        p2.score += k * (1 - prob2)
+    # Update the time
+    p1.time += 1
+    p2.time += 1
     game.queue.enqueue(p1) #Add to Queue
     game.queue.enqueue(p2)
+
+def export_txt(list):
+    with open('output.txt', 'w') as f:
+        i=0
+        for item in list:
+            f.write("Player %d: %s\n" % (i, item))
+            i+=1
+def export_csv(list):
+    with open('output.csv', 'w') as f:
+        f.write('ID,Initial Score,Current Score,Played Time\n')
+        i=0
+        for item in list:
+            f.write("%d,%d,%d,%d\n" % (i, item.init_score, item.score, item.time))
+            i+=1
--- main.py
+++ main.py
@@ -5,8 +5,12 @@ if __name__ == '__main__':
     player_num = 15
     random_numbers = np.random.normal(loc=1500, scale=500, size=player_num)
     for score in random_numbers:
-        lol.add_player(score)
+        lol.add_player(int(score))
     for player in lol.players:
         lol.queue.enqueue(player)
-    game.match()
-    print()
+    for _ in range(20):
+        lol.match()
+    for player in lol.players:
+        print(player)
+    game.export_txt(lol.players)
+    game.export_csv(lol.players)
