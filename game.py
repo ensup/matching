4c07f23e688b3b0c28b07fe82f4d09360aff63a4
Ensup Choi <ensup@vivaldi.net>
2025-02-11 00:10:25 +0900

    Add many methods

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 import random
-class QueueList(list): #class for List + Queue
+class QueueList(list):
+    """
+    class for List + Queue
+    """
     def enqueue(self, item):
         self.append(item)
     def dequeue(self):
@@ -9,21 +12,24 @@ class QueueList(list): #class for List + Queue
         for i in range(2, len(self)):
             if abs(self[i].score-self[0].score) < abs(self[idx].score-self[0].score):
                 idx = i
+        print(idx)
         return idx
-class Sys: #Class of game system
+class GameSys:
+    """
+    Class of game system
+    """
     def __init__(self):
         self.players = []
         self.queue = QueueList()
-        self.system_time = 0
+        self.system_time = 1  # 변수명 변경
     def add_player(self,score):
         self.players.append(Player(score))
     def add_queue(self):
-        self.system_time += 1
         for player in self.players:
-            if (player not in self.queue and (self.system_time >= player.std_time and self.system_time <= player.end_time
-                or player.std_time > player.end_time and self.system_time <= player.end_time)):
+            if player.is_available():
                 self.queue.enqueue(player)
                 print(f"Player ID {self.players.index(player)} added to queue at system time {self.system_time}")  # 디버깅 출력 추가
+        self.system_time += 1  # 변수명 변경
     def match(self):
         while len(self.queue) > 2:
             idx = self.queue.find_optimal()
@@ -31,20 +37,36 @@ class Sys: #Class of game system
             del self.queue[idx]
             player1 = self.queue.dequeue()
             print(f"Matched Player ID {self.players.index(player1)} and Player ID {self.players.index(player2)} at system time {self.system_time}")  # 디버깅 출력 추가
-            do_match((player1, player2), self)
+            do_match((player1, player2))
+    def get_time(self):
+        return self.system_time
+    def is_in_queue(self, player):
+        return player in self.queue
 
-class Player: #Save player data
+class Player:
+    """
+    Save player data
+    """
     def __init__(self, score):
         self.init_score = score
         self.score = score
         self.time = 0
-        self.std_time = random.randint(1, 24)
+        self.stt_time = random.randint(1, 24)
         self.end_time = random.randint(1, 24)
     def __str__(self):
         data = 'Initial score: ' + str(self.init_score) + ', Current score: ' + str(int(self.score)) + \
             ', Played time: ' + str(self.time)
         return data
-def do_match(players:tuple,game:Sys):
+    def is_available(self, game):
+        system_time = game.get_time()
+        return not game.is_in_queue(self) and self.in_time(system_time)
+    def in_time(self,system_time):
+        res = (self.stt_time <= system_time <= self.end_time
+                or self.stt_time > self.end_time >= system_time
+                or self.end_time < self.stt_time <= system_time <= 24)
+        return res
+
+def do_match(players:tuple):
     """
     Input: Tuple(Player, Player), Sys
     """
@@ -73,10 +95,10 @@ def export_txt(list):
             f.write("Player %d: %s\n" % (i, item))
             i+=1
 '''
-def export_csv(list):
+def export_csv(lst):
     with open('output.csv', 'w') as f:
         f.write('ID,Initial Score,Current Score,Played Time,PlayingTimeStart,PlayingTimeEnd\n')
         i=0
-        for item in list:
+        for item in lst:
             f.write("%d,%d,%d,%d,%d,%d\n" % (i, item.init_score, item.score, item.time, item.std_time, item.end_time))
             i+=1
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import game
 import numpy as np
 if __name__ == '__main__':
-    lol = game.Sys()
+    lol = game.GameSys()
     player_num = 15
     random_numbers = np.random.normal(loc=1500, scale=500, size=player_num)
     for score in random_numbers:
