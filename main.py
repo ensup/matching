f162a65b7951c27f6ff26729af478cd95bcd3363
Ensup Choi <ensup@vivaldi.net>
2025-02-12 21:10:54 +0900

    Expand users

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import game
 import numpy as np
 if __name__ == '__main__':
     lol = game.GameSys()
-    player_num = 15
+    player_num = 15000
     random_numbers = np.random.normal(loc=1500, scale=500, size=player_num)
     for score in random_numbers:
         lol.add_player(int(score))
